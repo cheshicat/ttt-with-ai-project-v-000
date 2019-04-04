dd499baf9aa2d85eb90b80198b4d7e1c4c7dab97
Allison Howard <alliemhow@gmail.com>
2019-04-04 07:59:45 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe_cli.rb
+++ lib/tic_tac_toe_cli.rb
@@ -6,6 +6,10 @@ class TicTacToeCLI
   def call
     puts "Welcome to Tic Tac Toe with AI!"
     input = ""
+
+    while input != "exit"
+     puts "What kind of game do you want to play - 0, 1, or 2 players? Please enter [0], [1], or [2]!"
+     num = gets.strip.to_i
   end
 
 end
