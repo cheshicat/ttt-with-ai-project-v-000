b555bc6abc34b35353617c69f3913f73e64d0eb4
Allison Howard <alliemhow@gmail.com>
2019-04-04 08:10:59 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -6,6 +6,7 @@ module Players
       #check if center cell avail & if yes take center
       #check if corners taken by opponent & if yes take opposite corner, if no then
         #check if corner taken by self & if yes, take opposite corner
+        #if no take corner
       #check if winning combo for self possible & if yes make winning move
       #check if winning combo for opponent possible & if yes block
 
