e2f16bb042080378d5184733fc5c49cbee277a55
Allison Howard <alliemhow@gmail.com>
2019-04-04 08:09:25 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,11 +2,11 @@ module Players
   class Computer < Player
 
     def move(board)
-      
+
       #check if center cell avail & if yes take center
-      #check if winning combo for self possible & if yes make winninh move
+      #check if winning combo for self possible & if yes make winning move
       #check if winning combo for opponent possible & if yes block
-      
+
     end
   end
 end
