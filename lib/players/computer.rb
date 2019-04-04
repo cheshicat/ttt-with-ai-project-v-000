46ac275cb55f21e50476d72ca3d81f908ff4e8ab
Allison Howard <alliemhow@gmail.com>
2019-04-04 08:18:47 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -20,6 +20,7 @@ module Players
         !board.taken?(combo[2]+1)) || ((board.cells[combo[1]] == token && board.cells[combo[2]] == token) &&
         !board.taken?(combo[0]+1)) || ((board.cells[combo[0]] == token && board.cells[combo[2]] == token) &&
         !board.taken?(combo[1]+1))
+      end
     end
 
       #check if corners taken by opponent & if yes take opposite corner, if no then
